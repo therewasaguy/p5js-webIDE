76db965dd84db8d44f6102f3b67f84a6deb20b2c
therewasaguy <jason.sigal@gmail.com>
2015-07-31 03:59:21 -0400

    reveal filetree in sidebar

--- app/sidebar/index.js
+++ app/sidebar/index.js
@@ -12,6 +12,34 @@ module.exports = {
 		sidebarWidth: undefined
 	},
 
+	components: {
+
+		file: {
+			template: require('./file.html'),
+			computed: {
+				hidden: function() {
+					return this.name[0] === '.';
+				},
+				icon: function() {
+					if (this.ext.match(/(png|jpg|gif|svg|jpeg)$/i)) return 'image';
+					else if (this.ext.match(/db$/i)) return 'db';
+					else return 'file';
+				},
+				className: function() {
+					var c = 'item';
+					if (this.$root.currentFile.name == this.name) c += ' selected';
+					return c;
+				}
+			},
+
+			methods: {
+				popupMenu: function(target, event) {
+					popupMenu.apply(this, arguments);
+				}
+			}
+		}
+	},
+
 	computed: {
 		className: function() {
 			var container = this.container || $('#sidebar-container');
