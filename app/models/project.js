985a95a2bd99a7529e01ea000a1d48e87eede4ac
therewasaguy <jason.sigal@gmail.com>
2015-07-31 02:27:36 -0400

    document the Project object

--- app/models/project.js
+++ app/models/project.js
@@ -1,20 +1,52 @@
 var pFile = require('./pFile');
 
-var Project = function(files) {
+// either load default file, or load new file;
+var Project = function(options) {
 
-	// generate random unique id, thank you https://gist.github.com/gordonbrander/2230317
-	this.id = '_' + Math.random().toString(36).substr(2, 9);
-	this.gistID = null;
-
-	// if no files are provided, set default files
-	if (!files) {
+	// if no options are provided, set default files
+	if (!options) {
+		/**
+		 *  @property {Array} files Array of pFile objects
+		 */
 		this.files = [ new pFile('p5.js'), new pFile('sketch.js', true), new pFile('index.html'), new pFile('style.css')];
+
+		/**
+		 *  [name description]
+		 *  @type {String}
+		 */
+		this.name = 'new project';
+
+		/**
+		 *  @property {String} openFile name of the file that is open
+		 */
+		this.openFile ='sketch.js';
+
+		/**
+		 *  @property {Array} openTabs 	Name of the openTabs, as array of strings
+		 */
+		this.openTabs = ['sketch.js', 'style.css'];
+
+		/**
+		 *  @property {String} id 	a unique ID for our application
+		 */
+		this.id = '_' + Math.random().toString(36).substr(2, 9);
+
+		/**
+		 *  @property {String} gistID 	a unique ID for GitHub API
+		 */
+		this.gistID = null;
 	}
 
-	this.name = 'new project';
-	this.openFile ='sketch.js';
-	this.openTabs = ['sketch.js', 'style.css'];
 
+	else {
+		this.files = options.files;
+		this.name = options.name;
+		this.openFile = options.openFile;
+		this.openTabs = options.openTabs;
+		this.dateModified = options.dateModified;
+		this.id = options.id;
+		this.gistID = options.gistID;
+	}
 
 	this.findFile = function(name) {
 		for (var i = 0; i < this.files.length; i++) {
