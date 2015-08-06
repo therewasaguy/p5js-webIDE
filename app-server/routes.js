a691251f8f47f365738bc56a54277a1a702cf179
therewasaguy <jason.sigal@gmail.com>
2015-08-06 04:11:46 -0400

    change default sketch template

--- app-server/routes.js
+++ app-server/routes.js
@@ -102,7 +102,7 @@ module.exports = function(app, passport) {
 	});
 
 	// TO DO...
-	app.get('/gist*', function(req, res) {
+	app.get('/gist/*', function(req, res) {
 		var urlSplit = req.url.split('gist/');
 		var gistID = urlSplit[1];
 
@@ -116,7 +116,7 @@ module.exports = function(app, passport) {
 		request(options, function(error, response, body) {
 			if (!error && response.statusCode == 200 || response.statusCode == 201) {
 				var data = JSON.parse(body);
-				res.render('index', data);
+				res.render('default');
 			  // res.send(data);
 			} else {
 				res.send(error);
